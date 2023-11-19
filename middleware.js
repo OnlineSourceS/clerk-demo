831c90effefd556d62fde96a8e252da2a9f3c24f
Muhammad Hamza Jawaid Shaikh <mrw58901878@gmail.com>
2023-11-18 22:45:48 -0800

    .

--- middleware.js
+++ middleware.js
@@ -1,9 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  // Set the paths that don't require the user to be signed in
-  // Sign in and sign up pages are already made public by Clerk
-  publicRoutes: ["/"],
+export default authMiddleware({debug: false
 });
 
 export const config = { matcher: "/((?!_next/image|_next/static|favicon.ico|.*.svg).*)" };
